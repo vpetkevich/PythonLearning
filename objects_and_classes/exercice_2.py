85f77aabae4e8c8a0b30d627586239d8ef0fd4eb
vpetkevich <62066192+vpetkevich@users.noreply.github.com>
2021-04-06 21:48:52 +0300

    Delete objects_and_classes directory

--- objects_and_classes/exercice_2.py
+++
@@ -1,8 +0,0 @@
-class Thing2:
-    letters = 'abc'
-
-
-thing = Thing2()
-
-print(thing.letters)
-print(Thing2.letters)
--- objects_and_classes/exercice_3.py
+++
@@ -1,6 +0,0 @@
-class Thing3:
-    pass
-
-
-Thing3.letters = 'xyz'
-print(Thing3.letters)
