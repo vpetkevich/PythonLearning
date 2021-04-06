86e7773b55834ce7694340881896a9dea726ac3a
vpetkevich <62066192+vpetkevich@users.noreply.github.com>
2021-04-06 21:48:23 +0300

    Delete dicts_and_sets directory

--- dicts_and_sets/exercice_13.py
+++
@@ -1,2 +0,0 @@
-print(dict(zip(('optimist', 'pessimist', 'troll'), ('The glass is half full', 'The glass is half empty',
-                                                    'How did you get the glass'))))
